ff0a62bca492e7b1899d88ef2c66b653d3329922
brocklarson <brock.larson0725@gmail.com>
2022-11-02 22:13:15 -0600

    Add menu page and function to open/close menu

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import {
-    headerSetup
+    headerSetup,
+    menuSetup,
+    createMenuLists,
 } from './page-setup.js';
 
 headerSetup();
+menuSetup();
+
+(function initEventListeners() {
+    const menuButton = document.getElementById(`menuButton`);
+    const menuContainer = document.getElementById(`menu`);
+    const menuBg = document.getElementById(`menuBg`);
+
+    menuButton.addEventListener(`click`, function() {
+        if (!menuContainer.classList.contains(`active`)) menuContainer.classList.add(`active`);
+        if (!menuBg.classList.contains(`active`)) menuBg.classList.add(`active`);
+    });
+    menuBg.addEventListener(`click`, function() {
+        if (menuContainer.classList.contains(`active`)) menuContainer.classList.remove(`active`);
+        if (menuBg.classList.contains(`active`)) menuBg.classList.remove(`active`);
+    });
+
+}());
